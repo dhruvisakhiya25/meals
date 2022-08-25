84a41fb3fd21444f62caacf42c82191514e9538f
dhruvisakhiya25 <dhr.globaliasoft@gmail.com>
2022-08-25 17:49:27 +0530

    first commit

--- lib/screens/shared_pref.dart
+++ lib/screens/shared_pref.dart
@@ -2,14 +2,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPref {
   static SharedPreferences? pref;
-  static String fbLoginName = 'login Name';
-  static String fbLoginEmail = 'login Email';
-  static String fbLoginPhoto = 'login Photo';
+  static String fbLoginName = 'login fbName';
+  static String fbLoginEmail = 'login fbEmail';
+  static String fbLoginPhoto = 'login fbPhoto';
+  static String googleLoginName = 'login googleName';
+  static String googleLoginEmail = 'login googleEmail';
+  static String googleLoginPhoto = 'login googlePhoto';
   static String imgKey = 'userName';
 
-
-
   static init() async => pref = await SharedPreferences.getInstance();
+
+  static set setProfileImage(value) => pref!.setString(imgKey, value);
+
+  static String? get getProfileImage => pref!.getString(imgKey);
 
   static set setFbLoginName(value) => pref!.setString(fbLoginName, value);
 
@@ -22,8 +27,4 @@ class SharedPref {
   static set setFbLoginPhoto(value) => pref!.setString(fbLoginPhoto, value);
 
   static String? get getFbLoginPhoto => pref!.getString(fbLoginPhoto);
-
-  static set setProfileImage(value) => pref!.setString(imgKey, value);
-
-  static String? get getProfileImage => pref!.getString(imgKey);
 }
