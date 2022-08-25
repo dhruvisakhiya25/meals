8b6d1d0b766f58b3ae6e66d0fa0e25387020fcc5
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-08-25 16:24:11 +0530

    f

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -87,7 +87,6 @@ class _NotificationPageState extends State<NotificationPage> {
                                 style: const TextStyle(color: grey),
                               ),
                             ),
-
                           ],
                         );
                       },
