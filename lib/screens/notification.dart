e98dd13ed343cab8cc90bce064b95425b50480ce
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-08-25 15:28:42 +0530

    f

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -15,59 +15,59 @@ class _NotificationPageState extends State<NotificationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      notification,
-                      style: const TextStyle(
-                          color: black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const Icon(icCart),
-                  ],
-                ),
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: notifications.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) => Column(
-                    children: [
-                      ListTile(
-                        leading: Container(
-                          height: 10,
-                          width: 10,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              color: orange),
-                        ),
-                        title: Text(
-                          notifications[index]['title'],
-                          style: const TextStyle(
-                              color: black, fontWeight: FontWeight.w400),
-                        ),
-                        subtitle: Text(
-                          notifications[index]['subTitle'],
-                          style: const TextStyle(color: grey),
-                        ),
-                      ),
-                      const Divider(
-                        color: grey,
-                        thickness: 0.3,
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+        // child: SafeArea(
+        //   child: Padding(
+        //     padding: const EdgeInsets.all(30.0),
+        //     child: Column(
+        //       children: [
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //           children: [
+        //             Text(
+        //               notification,
+        //               style: const TextStyle(
+        //                   color: black,
+        //                   fontSize: 25,
+        //                   fontWeight: FontWeight.bold),
+        //             ),
+        //             const Icon(icCart),
+        //           ],
+        //         ),
+        //         ListView.builder(
+        //           physics: const NeverScrollableScrollPhysics(),
+        //           itemCount: notifications.length,
+        //           shrinkWrap: true,
+        //           itemBuilder: (context, index) => Column(
+        //             children: [
+        //               ListTile(
+        //                 leading: Container(
+        //                   height: 10,
+        //                   width: 10,
+        //                   decoration: BoxDecoration(
+        //                       borderRadius: BorderRadius.circular(30),
+        //                       color: orange),
+        //                 ),
+        //                 title: Text(
+        //                   notifications[index]['title'],
+        //                   style: const TextStyle(
+        //                       color: black, fontWeight: FontWeight.w400),
+        //                 ),
+        //                 subtitle: Text(
+        //                   notifications[index]['subTitle'],
+        //                   style: const TextStyle(color: grey),
+        //                 ),
+        //               ),
+        //               const Divider(
+        //                 color: grey,
+        //                 thickness: 0.3,
+        //               )
+        //             ],
+        //           ),
+        //         )
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
