f0a14d94deb5d9e14ca2d61cecbb7bc91ba23598
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-08-25 16:19:20 +0530

    f

--- lib/screens/inbox.dart
+++ lib/screens/inbox.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:meals/utils/color.dart';
 import 'package:meals/utils/icon.dart';
@@ -11,73 +12,97 @@ class InBoxPage extends StatefulWidget {
 }
 
 class _InBoxPageState extends State<InBoxPage> {
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('inbox');
+  late Stream<QuerySnapshot> _streams;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _streams = _products.snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
                       inbox,
-                      style: const TextStyle(
-                          color: black,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
+                      style: const TextStyle(color: black, fontSize: 30),
                     ),
                     const Icon(
                       icCart,
                       color: black,
-                    )
+                    ),
                   ],
                 ),
-                ListView.separated(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 6,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) => ListTile(
-                    leading: Container(
-                      height: 10,
-                      width: 10,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(35),
-                          color: orange),
-                    ),
-                    title: Text(
-                      mealMonkeyPromotion,
-                      style: const TextStyle(
-                          color: black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [Text(dolor), Text(consectetur)],
-                    ),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(jul),
-                        const Icon(
-                          icStar,
-                          color: orange,
-                        )
-                      ],
-                    ),
-                  ),
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const SizedBox(
-                      height: 20,
-                    );
-                  },
-                ),
-              ],
-            ),
+              ),
+              StreamBuilder<QuerySnapshot>(
+                stream: _streams,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text(snapshot.error.toString()));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  QuerySnapshot querySnapshot = snapshot.data;
+                  List<QueryDocumentSnapshot> document = querySnapshot.docs;
+                  return ListView.separated(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 6,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      QueryDocumentSnapshot documents = document[index];
+                      return ListTile(
+                        leading: Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(35),
+                              color: orange),
+                        ),
+                        title: Text(
+                          documents['inbox meal'],
+                          style: const TextStyle(
+                              color: black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [Text(documents['inbox lorem']), Text(documents['inbox consectetur'])],
+                        ),
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(documents['inbox time']),
+                            const Icon(
+                              icStar,
+                              color: orange,
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(
+                        height: 20,
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:meals/utils/color.dart';
 import 'package:meals/utils/icon.dart';
@@ -11,93 +12,96 @@ class NotificationPage extends StatefulWidget {
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('notification');
+  late Stream<QuerySnapshot> _streams;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _streams = _products.snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        // child: SafeArea(
-        //   child: Padding(
-        //     padding: const EdgeInsets.all(30.0),
-        //     child: Column(
-        //       children: [
-        //         Row(
-        //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //           children: [
-        //             Text(
-        //               notification,
-        //               style: const TextStyle(
-        //                   color: black,
-        //                   fontSize: 25,
-        //                   fontWeight: FontWeight.bold),
-        //             ),
-        //             const Icon(icCart),
-        //           ],
-        //         ),
-        //         ListView.builder(
-        //           physics: const NeverScrollableScrollPhysics(),
-        //           itemCount: notifications.length,
-        //           shrinkWrap: true,
-        //           itemBuilder: (context, index) => Column(
-        //             children: [
-        //               ListTile(
-        //                 leading: Container(
-        //                   height: 10,
-        //                   width: 10,
-        //                   decoration: BoxDecoration(
-        //                       borderRadius: BorderRadius.circular(30),
-        //                       color: orange),
-        //                 ),
-        //                 title: Text(
-        //                   notifications[index]['title'],
-        //                   style: const TextStyle(
-        //                       color: black, fontWeight: FontWeight.w400),
-        //                 ),
-        //                 subtitle: Text(
-        //                   notifications[index]['subTitle'],
-        //                   style: const TextStyle(color: grey),
-        //                 ),
-        //               ),
-        //               const Divider(
-        //                 color: grey,
-        //                 thickness: 0.3,
-        //               )
-        //             ],
-        //           ),
-        //         )
-        //       ],
-        //     ),
-        //   ),
-        // ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        notification,
+                        style: const TextStyle(color: black, fontSize: 30),
+                      ),
+                      const Icon(
+                        icCart,
+                        color: black,
+                      ),
+                    ],
+                  ),
+                ),
+                StreamBuilder<QuerySnapshot>(
+                  stream: _streams,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(child: Text(snapshot.error.toString()));
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    QuerySnapshot querySnapshot = snapshot.data;
+                    List<QueryDocumentSnapshot> document = querySnapshot.docs;
+                    return ListView.separated(
+                      separatorBuilder: (context, index) => const Divider(
+                        height: 3,
+                      ),
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: document.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        QueryDocumentSnapshot documents = document[index];
+                        return Column(
+                          children: [
+                            ListTile(
+                              leading: Container(
+                                height: 10,
+                                width: 10,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: orange),
+                              ),
+                              title: Text(
+                                documents['notification txt'],
+                                style: const TextStyle(
+                                    color: black, fontWeight: FontWeight.w400),
+                              ),
+                              subtitle: Text(
+                                documents['notification time'],
+                                style: const TextStyle(color: grey),
+                              ),
+                            ),
+                            const Divider(
+                              color: grey,
+                              thickness: 0.3,
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
-List notifications = [
-  {'title': 'Your order has been piked up', 'subTitle': 'Now'},
-  {'title': 'Your order has been delivered', 'subTitle': '1 h ago'},
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '3 h ago'
-  },
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '5 h ago'
-  },
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '05 sep 2020'
-  },
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '12 Aug 2020'
-  },
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '20 Jul 2020'
-  },
-  {
-    'title': 'Lorem Ipsum dolor slt amet consectetur3 h ago',
-    'subTitle': '12 jul 2020'
-  }
-];
