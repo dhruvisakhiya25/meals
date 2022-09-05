9672aec649888c0d4afab7b099329f53bef7c4b6
iMac <imac@192.168.0.142>
2022-09-05 16:22:04 +0530

    f
    f

---
+++ lib/screens/CartPages/cart.dart
@@ -0,0 +1,20 @@
+class Product {
+  // Product's variables : name , price , imageUrl . All required .
+  final String name;
+  final double price;
+  final String imageUrl;
+  const Product({
+    required this.name,
+    required this.price,
+    required this.imageUrl,
+  });
+}
+
+List products = [
+  const Product(
+    name: 'Pizza',
+    price: 100,
+    imageUrl:
+        'https://images.unsplash.com/photo-1593504049359-74330189a345?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+  ),
+];
