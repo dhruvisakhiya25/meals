fec6ceea665f9647d086eac591354df1246c28c1
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-09-12 17:15:27 +0530

    first commit

--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -1,6 +1,5 @@
 import 'package:meals/Network/export.dart';
 class Screens {
   static double height(context) => MediaQuery.of(context).size.height;
-
   static double width(context) => MediaQuery.of(context).size.width;
 }
