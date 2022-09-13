ede47160856dee9ec8b686b9afe829823bff12fb
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-09-13 12:20:41 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,6 @@ class MyApp extends StatelessWidget {
         ), GetPage(
           name: '/iteamsandwich',
           page: () => const Sandwich(),
-
         ), GetPage(
           name: '/iteampasta',
           page: () => const Pasta(),
