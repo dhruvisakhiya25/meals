454edcff52344ef376918de80cdf30e4d898e5e7
dhruvisakhiya25 <dhr.globaliasoft@gmail.com>
2022-09-14 14:52:15 +0530

    new

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -8,17 +8,12 @@
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:google_sign_in_android/google_sign_in_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
-import 'package:path_provider_android/path_provider_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:google_sign_in_ios/google_sign_in_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
-import 'package:path_provider_ios/path_provider_ios.dart';
 import 'package:shared_preferences_ios/shared_preferences_ios.dart';
-import 'package:path_provider_linux/path_provider_linux.dart';
 import 'package:shared_preferences_linux/shared_preferences_linux.dart';
-import 'package:path_provider_macos/path_provider_macos.dart';
 import 'package:shared_preferences_macos/shared_preferences_macos.dart';
-import 'package:path_provider_windows/path_provider_windows.dart';
 import 'package:shared_preferences_windows/shared_preferences_windows.dart';
 
 @pragma('vm:entry-point')
@@ -42,16 +37,6 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`image_picker_android` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-        rethrow;
-      }
-
-      try {
-        PathProviderAndroid.registerWith();
-      } catch (err) {
-        print(
-          '`path_provider_android` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
         rethrow;
@@ -89,16 +74,6 @@ class _PluginRegistrant {
       }
 
       try {
-        PathProviderIOS.registerWith();
-      } catch (err) {
-        print(
-          '`path_provider_ios` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-        rethrow;
-      }
-
-      try {
         SharedPreferencesIOS.registerWith();
       } catch (err) {
         print(
@@ -109,16 +84,6 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isLinux) {
-      try {
-        PathProviderLinux.registerWith();
-      } catch (err) {
-        print(
-          '`path_provider_linux` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-        rethrow;
-      }
-
       try {
         SharedPreferencesLinux.registerWith();
       } catch (err) {
@@ -131,16 +96,6 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
-        PathProviderMacOS.registerWith();
-      } catch (err) {
-        print(
-          '`path_provider_macos` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-        rethrow;
-      }
-
-      try {
         SharedPreferencesMacOS.registerWith();
       } catch (err) {
         print(
@@ -151,16 +106,6 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isWindows) {
-      try {
-        PathProviderWindows.registerWith();
-      } catch (err) {
-        print(
-          '`path_provider_windows` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-        rethrow;
-      }
-
       try {
         SharedPreferencesWindows.registerWith();
       } catch (err) {
