e3c9ee4e42e76ce47fadfdf0d29874f5ada04d6d
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-09-14 15:23:13 +0530

    first commit

--- lib/screens/Home/food_type/chinese.dart
+++ lib/screens/Home/food_type/chinese.dart
@@ -9,11 +9,10 @@ class Chinese extends StatefulWidget {
 
 class _ChineseState extends State<Chinese> {
   final CollectionReference _products =
-  FirebaseFirestore.instance.collection('chinese');
+      FirebaseFirestore.instance.collection('chinese');
   late Stream<QuerySnapshot> _streams;
 
   @override
-
   void initState() {
     super.initState();
     _streams = _products.snapshots();
@@ -66,7 +65,6 @@ class _ChineseState extends State<Chinese> {
                                 fit: BoxFit.cover),
                           ),
                         ),
-
                       ],
                     );
                   },
