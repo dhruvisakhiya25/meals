8bec131a0ea49c23199f5732a9bfb6b9fe739841
yash123sakhiya <yas.globaliasoft@gmail.com>
2022-09-20 14:29:28 +0530

    first commit

--- lib/Network/authController.dart
+++ lib/Network/authController.dart
@@ -27,18 +27,13 @@ class RegisterController extends GetxController {
           email: email, password: password);
       Get.offAllNamed('/mainPage');
     } on FirebaseAuthException catch (e) {
-      // this is solely for the Firebase Auth Exception
-      // for example : password did not match
       print(e.message);
-      // Get.snackbar("Error", e.message!);
       Get.snackbar(
         "Error",
         e.message!,
         snackPosition: SnackPosition.BOTTOM,
       );
     } catch (e) {
-      // this is temporary. you can handle different kinds of activities
-      //such as dialogue to indicate what's wrong
       print(e.toString());
       Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
     }
@@ -48,8 +43,6 @@ class RegisterController extends GetxController {
     try {
       await auth.signInWithEmailAndPassword(email: email, password: password);
     } on FirebaseAuthException catch (e) {
-      // this is solely for the Firebase Auth Exception
-      // for example : password did not match
       print(e.message);
     } catch (e) {
       print(e.toString());
@@ -68,7 +61,7 @@ class RegisterController extends GetxController {
           snackPosition: SnackPosition.BOTTOM,
           backgroundColor: Colors.orange,
           colorText: Colors.black);
-      update(); // <-- without this the isSigned value is not updated.
+      update();
     } catch (e) {
       Get.snackbar('Login Failed', 'Please Try Again',
           snackPosition: SnackPosition.BOTTOM,
@@ -119,7 +112,6 @@ class RegisterController extends GetxController {
           snackPosition: SnackPosition.BOTTOM,
           backgroundColor: white,
           colorText: black);
-      // Get.offAllNamed('/loginPage');
     } catch (e) {
       Get.snackbar('Error occurred!', e.toString(),
           snackPosition: SnackPosition.BOTTOM,
@@ -128,13 +120,6 @@ class RegisterController extends GetxController {
     }
   }
 
-  // void signOut() {
-  //   try {
-  //     auth.signOut();
-  //   } catch (e) {
-  //     print(e.toString());
-  //   }
-  // }
   signOut() async {
     try {
       await auth.signOut();
